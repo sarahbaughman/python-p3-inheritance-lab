25762d8e036f703d142db09b934826f3a619c16d
Sarah Baughman <sarahbaughman92@gmail.com>
2023-05-03 15:05:27 -0400

    finished lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [ ]
+
+# Knowledge is not in the parenthesis because they aren't starting with any knowledge, it's not a given like their name
+    def learn(self, string):
+        self.knowledge.append(string)
